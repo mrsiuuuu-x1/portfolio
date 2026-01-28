90e53aa7f5c02481a49e4463ec973d0cf350a9e8
Abdul Rafay <abdulrafay88243@gmail.com>
2026-01-28 19:26:26 +0500

    feat: added language bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const playfair = Playfair_Display({ subsets: ["latin"], variable:'--font-playfair'})
 
 export const metadata: Metadata = {
   title: "Abdul Rafay | Portfolio",
@@ -17,8 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar /> {/* <--- 2. Add it here */}
+      <body className={`${inter.variable} ${playfair.variable} font-sans`}>
+        <Navbar />
         {children}
       </body>
     </html>
